ade22b2959240997d9438e1d6418a491bb657e3b
abhay2132 <abhaybro768@gmail.com>
2022-04-22 08:46:20 +0530

    dev

--- src/static/public/js/loadFiles.js
+++ src/static/public/js/loadFiles.js
@@ -1,15 +1,15 @@
 const cacheF = {
 	files : [],
-	av : localStorage.getItem("fileV") || 0,
+	av : parseInt(localStorage.getItem("fileV") || "0"),
 	getF : function (url, f ) { return new Promise( res => {
 		if ( f ) fetch(url)
 					.then(d => d.text())
-					.then(t => localStorage.setItem(url, t, res(t)))
-		else res(localStorage.getItem(url) || "" )
+					.then(t => localStorage.setItem(url, t, res(t), this.getM = "L"))
+		else {res(localStorage.getItem(url) || "" ); this.getM = "C";}
 	})},
 	logs : [],
 	init : async function ({logging = false, appV = 0, ignore = [], cb = () =>{}, uc = false}) { // use cache
-		this.logs = [];
+		this.logs = ["appV : "+appV ];
 		let sT = Date.now();
 		let f = !appV || this.av != appV
 		if ( uc ) f = !1;
@@ -19,9 +19,10 @@ const cacheF = {
 			let data = await this.getF( file , f);
 			if ( file.endsWith(".js")) eval(data);
 			else if ( file.endsWith(".css")) document.body.innerHTML += `<style>${data}</style>`;
-			this.logs.push(file.split("/").at(-1) + " "+(Date.now() - st) +"ms")
+			this.logs.push(this.getM + " : " + file.split("/").at(-1) + " "+(Date.now() - st) +"ms")
 		}
 		if (logging ) (this.logs.push("Total Time : "+ (Date.now() - sT) +"ms"), console.log(this.logs))
+
 		localStorage.setItem("fileV", appV)
 		if (typeof cb == "function") return cb()
 		return;
