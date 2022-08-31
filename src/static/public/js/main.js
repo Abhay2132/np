321b430b952c78cae72c16a31a73738f01bd03ae
abhay2132 <abhaybro768@gmail.com>
2022-08-31 08:03:42 +0530

    fixed uc-menu

--- src/static/public/js/main.js
+++ src/static/public/js/main.js
@@ -3,11 +3,15 @@
 
 	window.t_uc = {
 		uc : $(".uc-menu"),
-		off : function(){
+		off : async function(){
 			this.uc.style.opacity = 0;
 			this.uc.style.transform = "translateY(20px)";
+			await wait(300);
+			this.uc.style.display = 'none';
 		},
-		on : function(){
+		on : async function(){
+			this.uc.style.display = 'block';
+			await wait(1);
 			this.uc.style.opacity = 1;
 			this.uc.style.transform = "translateY(0px)";
 		},
