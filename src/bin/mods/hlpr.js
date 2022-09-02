0280db30aca0ba7156d9b4e2e9b511217ba97514
abhay2132 <abhaybro768@gmail.com>
2022-09-02 09:39:22 +0530

    reverted to old ine line logging in routes

--- src/bin/mods/hlpr.js
+++ src/bin/mods/hlpr.js
@@ -17,7 +17,7 @@ const fs = require("fs"),
 	
 const logger = (req, res, next) => {
 		if ( req.url == "/reload") return //log({reload})
-		let st = Date.now();
+		let st = performance.now();
 		let mc = {
 			GET: "green",
 			POST: "yellow",
@@ -26,10 +26,10 @@ const logger = (req, res, next) => {
 		};
 		let clr = mc[req.method] || "grey";
 		clr = colors[clr](req.method)
-		process.stdout.write(req.url+" ");
+		//process.stdout.write(req.url+" ");
 		res.on("finish", () => {
 		let method = res.statusCode >= 400 ? colors.bgRed(clr)  : clr
-		log(method, colors.yellow(Date.now() - st + "ms"))
+		log(req.url, method, colors.yellow((performance.now() - st).toFixed(2) + "ms"))
 		})
 		next();
 	}
