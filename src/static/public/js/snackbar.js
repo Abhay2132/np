b8827d04b386eec1db33ae33dba6f63fda0ec7bf
abhay2132 <abhaybro768@gmail.com>
2022-10-25 18:52:58 +0530

    fixed snackbar loading and night theme

--- src/static/public/js/snackbar.js
+++ src/static/public/js/snackbar.js
@@ -1,21 +1,27 @@
 (async function() {
+	const wid = window.innerWidth * 0.4;
 	window.snkbr = {
 		cb: false,
 		hideAfter : 5000,
 		timeout : false,
-		async show(text, hideAfter, cb) {
+		ps : [],
+		killed : false,
+		async show(text, hideAfter=this.hideAfter, cb) {
 			if ($('.snackbar-container').style.display== "block") return ;
+			const bdr = $('.snackbar-border');
 			$('.sb-body').textContent = text;
+			this.text = text;
 			$('.snackbar-container').style.display= "block";
-			$('.snackbar-border').style.animationDuration = hideAfter+"ms";
+			bdr.style.strokeDashoffset = length;
+			bdr.style.animationName = "dash";
+			bdr.style.animationDuration = hideAfter+"ms";
 			await wait(10);
 
 			$('.snackbar').style.bottom = '0px';
 
 			cb && (this.cb = cb);
-			await wait(hideAfter || this.hideAfter);
-			this.hide();
-			// hideAfter && (this.timeout = setTimeout(this.hide, hideAfter));
+			this.timeout = setTimeout(this.hide, hideAfter);
+			await wait(hideAfter);
 		},
 		async hide() {
 			(!!this.timeout && clearTimeout(this.timeout));
@@ -24,24 +30,26 @@
 			(!!this.cb && this.cb()) || (this.cb = false);
 			await wait(300);
 			$('.snackbar-container').style.display= "none";
+			$(".snackbar-border").style.animationName = "abhay"
+
 		}
 	}
-	const w = window.innerWidth - 4;
-	const fs = 18;
 
+	const fs = 18;
 	const svg = `
-<svg width='${w}' height="40">
-<path class="snackbar-border"
-d="M 1 1 , H ${w-1} , V 39 , H 1 , L 1 1 " 
-/>
-<text x='14' y='${17+fs/2}'
-	style="font:normal ${fs}px arial; stroke-width:0; fill : #333;"
- class="sb-body"></text>
-</svg>
+		<svg width='' height="40" id='snackbar-svg'>
+		<path class="snackbar-border"
+			d="M 1 1 , H ${wid-1} , V 39 , H 1 , L 1 1 " 
+		/>
+		<text x='14' y='${17+fs/2}'
+			style="font:normal ${fs}px arial; stroke-width:0;"
+ 			class="sb-body"></text>
+		</svg>
 	`
 	$(".snackbar").innerHTML = svg;
-
-	await wait(5);
+	$(".snackbar").style.width = wid + "px";
+	$("#snackbar-svg").setAttribute("width", wid)
+	await wait(50);
 	const length = $('.snackbar-border').getTotalLength();
 	console.table({length})
 	document.body.innerHTML += `
@@ -60,14 +68,17 @@ d="M 1 1 , H ${w-1} , V 39 , H 1 , L 1 1 "
 	`
 	await wait(10);
 	const serverOnline = window.sessionStorage.getItem("serverOnline");
-	if (!0) {
+	if (!serverOnline) {
 		log("Starting SERVER !")
 		snkbr.show("Starting SERVER ...", 60000);
+		await wait(2000);
 		fetch("/")
 		.then(async a => {
+			await snkbr.hide();
 			await snkbr.hide();
 			snkbr.show("Server ONLINE ;D", 2000);
 			window.sessionStorage.setItem("serverOnline", true);
 		})
 	}
+
 })();
